7c74b6915bfb1f9ec2526e7121ddd80d15b206aa
clebsonc <clebsondm@gmail.com>
2017-02-20 18:43:37 -0300

    bootcamp maximum sum

--- chap17_dynamic_programming/findMaximumSumBF.cpp
+++ chap17_dynamic_programming/findMaximumSumBF.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <limits>
+#include <cmath>
 
 /**
 * Find the subarray containing the maximum sum of elements of a given array.
-* Prints out the maximum value of the subarray and return the pair container 
+* Return the maximum subarray value indexes in the container pair
 * with the member first and second having the start and end index of the 
 * subarrays.
 */
-std::pair<int, int> findMaximumSubArrayBruteForce(const std::vector<int> & v){
-  int max = v.at(0);
+std::pair<int, int> findMaximumSubArrayBruteForce(const std::vector<int> & v, int & max){
+  max = v.at(0);
   std::pair<int, int> index(0,0);
   for(int i = 0; i < v.size(); i++){
     int sum = 0;
@@ -23,14 +25,39 @@ std::pair<int, int> findMaximumSubArrayBruteForce(const std::vector<int> & v){
     } // end for
   } // end for
   
-  std::cout << "Max: " << max << std::endl;
   return index;
 }
 
+std::pair<int, int> findMaximumSubArrayDynamicProgramming(std::vector<int> v, int & val){
+  int m = std::numeric_limits<int>::min()/2;
+  int s = 0;
+  int e = 0;
+  for (int i = 0; i < v.size(); i++){
+    if(m+v.at(i) > v.at(i)){
+      m = m+v.at(i);
+      e = i;
+    } 
+    else{
+      m = v.at(i);
+      s = i;
+      e = i;
+    }
+  }
+  val = m;
+  std::pair<int, int> p(s, e);
+  return p;
+}
+
 int main(){
-  std::vector<int> v = {-904, 40, 523, 12, -335, -385, -124, 481, -31};
-  std::pair<int, int> p = findMaximumSubArrayBruteForce(v);
-  std::cout << "Subarray: ["<< p.first << ", " << p.second << "]" << std::endl;
+  //std::vector<int> v = {-904, 40, 523, 12, -335, -385, -124, 481, -31};
+  std::vector<int> v = {-20, 10, -1, 13};
+  //std::vector<int> v = {-904, 40, 523, 12, -335};
+  int max = 0;
+  std::pair<int, int> p = findMaximumSubArrayBruteForce(v, max);
+  std::cout << "Subarray: ["<< p.first << ", " << p.second << "] -- " << max << std::endl;
+  max = 0;
+  std::pair<int, int> t = findMaximumSubArrayDynamicProgramming(v, max);
+  std::cout << "Subarray: ["<< t.first << ", " << t.second << "] -- " << max << std::endl;
 
   return 0;
 }
