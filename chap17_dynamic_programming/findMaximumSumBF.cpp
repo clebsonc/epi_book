8f92a7145e0f89a20bd5cb1f5fee2783bf3222b2
clebsonc <clebsondm@gmail.com>
2017-05-09 23:28:30 -0300

    adding memoization approach

--- chap17_dynamic_programming/findMaximumSumBF.cpp
+++ chap17_dynamic_programming/findMaximumSumBF.cpp
@@ -10,12 +10,13 @@
 * with the member first and second having the start and end index of the 
 * subarrays.
 */
-std::pair<int, int> findMaximumSubArrayBruteForce(const std::vector<int> & v, int & max){
+std::pair<int, int> findMaximumSubArrayBruteForce(const std::vector<int> & v, 
+                                                  int & max){
   max = v.at(0);
   std::pair<int, int> index(0,0);
-  for(int i = 0; i < v.size(); i++){
+  for(int i = 0; i < static_cast<int>(v.size()); i++){
     int sum = 0;
-    for(int j = i; j<v.size(); j++){
+    for(int j = i; j < static_cast<int>(v.size()); j++){
       sum += v.at(j);
       if(sum > max){
         index.first = i;
@@ -28,11 +29,41 @@ std::pair<int, int> findMaximumSubArrayBruteForce(const std::vector<int> & v, in
   return index;
 }
 
-std::pair<int, int> findMaximumSubArrayDynamicProgramming(std::vector<int> v, int & val){
+
+std::pair<int, int> maximumValueMemoDP(std::vector<std::vector<int>> & memo,
+                                       int & max){
+  std::pair<int, int> index(0, 0);
+  for (int i = 0; i < static_cast<int>(memo.size())-1; i++){
+    for (int j = i+1; j < static_cast<int>(memo.size()); j++){
+      memo.at(i).at(j) = memo.at(i).at(j-1) + memo.at(j).at(j);
+      if (memo.at(i).at(j) > max){
+        max = memo.at(i).at(j);
+        index.first = i;
+        index.second = j;
+      }
+    }
+  }
+  return index;
+}
+
+
+std::pair<int, int> findMaximumSubArrayMemoization(const std::vector<int> & v, 
+                                                  int & val){
+  std::vector<std::vector<int>> memo(v.size(), std::vector<int>(v.size(), 0));
+  // initialize memo table
+  for (int i = 0; i < static_cast<int>(v.size()); i++){
+    memo.at(i).at(i) = v.at(i);
+  }
+  return maximumValueMemoDP(memo, val);
+}
+
+
+std::pair<int, int> findMaximumSubArrayDynamicProgramming(std::vector<int> v, 
+                                                          int & val){
   int m = std::numeric_limits<int>::min()/2;
   int s = 0;
   int e = 0;
-  for (int i = 0; i < v.size(); i++){
+  for (int i = 0; i < static_cast<int>(v.size()); i++){
     if(m+v.at(i) > v.at(i)){
       m = m+v.at(i);
       e = i;
@@ -49,15 +80,21 @@ std::pair<int, int> findMaximumSubArrayDynamicProgramming(std::vector<int> v, in
 }
 
 int main(){
-  //std::vector<int> v = {-904, 40, 523, 12, -335, -385, -124, 481, -31};
-  std::vector<int> v = {-20, 10, -1, 13};
+  std::vector<int> v = {904, 40, 523, 12, -335, -385, -124, 481, -31};
+  //std::vector<int> v = {-20, 10, -1, 13};
   //std::vector<int> v = {-904, 40, 523, 12, -335};
   int max = 0;
   std::pair<int, int> p = findMaximumSubArrayBruteForce(v, max);
-  std::cout << "Subarray: ["<< p.first << ", " << p.second << "] -- " << max << std::endl;
+  std::cout << "Sub array: ["<< p.first << ", " << p.second << "] -- " << max << std::endl;
   max = 0;
   std::pair<int, int> t = findMaximumSubArrayDynamicProgramming(v, max);
-  std::cout << "Subarray: ["<< t.first << ", " << t.second << "] -- " << max << std::endl;
+  std::cout << "Sub array: ["<< t.first << ", " << t.second << "] -- " << max << std::endl;
+
+  max = 0;
+  std::pair<int, int> r = findMaximumSubArrayMemoization(v, max);
+  std::cout << "Sub array: ["<< r.first << ", " << r.second << "] -- " << max << std::endl;
+  
+
 
   return 0;
 }
